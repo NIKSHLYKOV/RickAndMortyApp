fcf6b9d26ea99f4365be915f1a1724e530c438b0
Nik Shlykov <nikshlykovdev@gmail.com>
2021-06-26 16:15:02 +0300

    add sample layout for list element and change theme and colors

--- app/src/main/java/ru/nikshlykov/rickandmortyapiapp/ui/activities/MainActivity.kt
+++ app/src/main/java/ru/nikshlykov/rickandmortyapiapp/ui/activities/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
     val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
     val navController = findNavController(R.id.nav_host_fragment)
-    // Passing each menu ID as a set of Ids because each
+    /*// Passing each menu ID as a set of Ids because each
     // menu should be considered as top level destinations.
     val appBarConfiguration = AppBarConfiguration(
       setOf(
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         R.id.navigation_notifications
       )
     )
-    setupActionBarWithNavController(navController, appBarConfiguration)
+    setupActionBarWithNavController(navController, appBarConfiguration)*/
     navView.setupWithNavController(navController)
   }
 }
